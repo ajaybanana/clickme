9748c0ef305b82e50d395bd4e41ff9adbbb7bef3
ajaybanana <110432485+ajaybanana@users.noreply.github.com>
2023-10-06 10:59:17 -0700

    initial commit
    Look ma I made a button that counts and changes colors

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  const [clickCount, setClickCount] = useState(0);
+  const [darkMode, setDarkMode] = useState(false);
+
+  const handleButtonClick = () => {
+      setClickCount(clickCount + 1);
+     if (clickCount > 7 && (clickCount % 7) == 0) {
+      setDarkMode(true);
+     setClickCount(clickCount + 1); 
+    } else {
+      setDarkMode(false);
+    }
+  };
+  const handleStartOver = () => {
+    setClickCount(0);
+    setDarkMode(false);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={`App ${darkMode ? 'dark-mode' : 'light-mode'}`}>
+      <h1>Click Count: {clickCount}</h1>
+      <button onClick={handleButtonClick}>Click Me</button>
+      <button onClick={handleStartOver}>Start Over</button>
     </div>
   );
 }
 
 export default App;
+
